d376da8aaf14caf8d15718e81f0bd16b90fa60de
Scott Latunski <latunsk4@gmail.com>
2019-05-23 23:26:57 -0400

    fix repositories

--- src/main/java/guru/springframework/spring5webapp/repositories/BookRepositories.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepositories.java
@@ -6,59 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public class BookRepositories implements CrudRepository<Author, Long> {
-    @Override
-    public <S extends Author> S save(S s) {
-        return null;
-    }
-
-    @Override
-    public <S extends Author> Iterable<S> saveAll(Iterable<S> iterable) {
-        return null;
-    }
-
-    @Override
-    public Optional<Author> findById(Long aLong) {
-        return Optional.empty();
-    }
-
-    @Override
-    public boolean existsById(Long aLong) {
-        return false;
-    }
-
-    @Override
-    public Iterable<Author> findAll() {
-        return null;
-    }
-
-    @Override
-    public Iterable<Author> findAllById(Iterable<Long> iterable) {
-        return null;
-    }
-
-    @Override
-    public long count() {
-        return 0;
-    }
-
-    @Override
-    public void deleteById(Long aLong) {
-
-    }
-
-    @Override
-    public void delete(Author author) {
-
-    }
-
-    @Override
-    public void deleteAll(Iterable<? extends Author> iterable) {
-
-    }
-
-    @Override
-    public void deleteAll() {
-
-    }
+//Spring auto-generates the methods
+public interface BookRepositories extends CrudRepository<Author, Long> {
 }
